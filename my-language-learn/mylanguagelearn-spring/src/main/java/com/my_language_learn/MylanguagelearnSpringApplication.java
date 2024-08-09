0220a9a622a1cff55f1df7ef95a5ba3c5eb9a0c0
luishenrique <luishenrrique34@gmail.com>
2024-08-09 11:31:58 -0300

    inicia projeto utilizando spring para as anotações java no back-end

---
+++ my-language-learn/mylanguagelearn-spring/src/main/java/com/my_language_learn/MylanguagelearnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.my_language_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylanguagelearnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylanguagelearnSpringApplication.class, args);
+	}
+
+}
